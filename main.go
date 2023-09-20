fd1c3ae254207930072ca88b46361aece3d4a3b2
Amirabbas Fazelinia <utilyre@proton.me>
2023-09-20 11:54:59 +0330

    fix!: simplify the tool by only serving one file or stdin

--- main.go
+++ main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"fmt"
-	"io/fs"
-	"net"
+	"io"
 	"net/http"
 	"os"
 )
 
-var (
-	ErrNotEnoughArguments = errors.New("not enough arguments")
-	ErrIPNotFound         = errors.New("IP not found")
-)
+var ErrTooManyArguments = errors.New("too many arguments")
 
 func main() {
 	if err := run(); err != nil {
@@ -23,80 +20,34 @@ func main() {
 }
 
 func run() error {
-	port := flag.String("port", "80", "TCP port for the server to listen on")
+	addr := flag.String("o", "0.0.0.0:8080", "TCP address for the server to listen on")
 	flag.Parse()
-	if flag.NArg() != 1 {
-		return ErrNotEnoughArguments
+	if flag.NArg() > 1 {
+		return ErrTooManyArguments
 	}
 
 	mux := http.NewServeMux()
 	srv := http.Server{
-		Addr:    ":" + *port,
+		Addr:    *addr,
 		Handler: mux,
 	}
 
-	name := flag.Arg(0)
-	info, err := getFileStat(name)
-	if err != nil {
-		return err
-	}
-
-	switch mode := info.Mode(); {
-	case mode.IsRegular():
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			if r.URL.Path != "/" {
-				http.NotFound(w, r)
-				return
-			}
-
-			http.ServeFile(w, r, name)
-		})
-	case mode.IsDir():
-		mux.Handle("/", http.FileServer(http.Dir(name)))
-	default:
-		return fmt.Errorf("file mode '%s' not supported", mode)
-	}
-
-	ip, err := getLocalIP()
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Local http://localhost:%s\n", *port)
-	fmt.Printf("Network http://%s:%s\n", ip, *port)
-
-	return srv.ListenAndServe()
-}
-
-func getFileStat(name string) (fs.FileInfo, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	return f.Stat()
-}
-
-func getLocalIP() (net.IP, error) {
-	host, err := os.Hostname()
-	if err != nil {
-		return nil, err
-	}
-
-	ips, err := net.LookupIP(host)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, ip := range ips {
-		ipv4 := ip.To4()
-		if ipv4 == nil {
-			continue
+	if flag.NArg() == 0 {
+		stdin, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			return err
 		}
 
-		return ip, nil
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Accept-Ranges", "bytes")
+			io.Copy(w, bytes.NewReader(stdin))
+		})
+	} else {
+		name := flag.Arg(0)
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, name)
+		})
 	}
 
-	return nil, ErrIPNotFound
+	return srv.ListenAndServe()
 }
